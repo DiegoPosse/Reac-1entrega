beacee9e0378d4f0148d19f33ece9200fc539846
DiegoPosse <diegoposse79@gmail.com>
2023-07-24 14:38:40 -0300

    Diego-Posse-entrega2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
 import Navbar from './componentes/NavBar/NavBar'
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './componentes/ItemdetailContainer/ItemDetailContainer'
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
+import Inicio from './pages/Inicio';
+import Detalle from './pages/Detalle';
+import Nosotros from './pages/Nosotros';
 import './App.css'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <div className='Proyecto'>
-        
-        <Navbar/>
-        <ItemListContainer greeting='Lista de Productos'/>
-       
-        
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <main id='main__container'>
+      <Routes>
+        <Route
+          exact
+          path='Inicio'
+          element={<Inicio/>}
+        />
+        <Route
+          path='Detalle/:id'
+          element={<Detalle/>}
+        />
+        <Route
+          path='Nosotros'
+          element={<Nosotros/>}
+        />
+      </Routes>
+    </main>
+  </BrowserRouter>
+   // <div className='Proyecto'>
+   //     
+   //     <Navbar/>
+   //     <ItemListContainer greeting='Lista de Productos'/>
+   //    
+    //    
+    //</div>
 
       
      
   )
 }
-<ItemDetailContainer/>
+
 export default App
---
+++ src/componentes/pages/Inicio/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ItemListContainer from '../../ItemListContainer/ItemListContainer'
+const Inicio = () => {
+  return (
+    <div className='Main-container'>
+      <ItemListContainer greeting='Lista de Productos'/>
+    </div>
+  
+    )
+}
+export default Inicio
